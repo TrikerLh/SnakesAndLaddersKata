08f088c7b76fe81255bfb2386e8b8d1ecb8c2170
TrikerLh <corchero100@gmail.com>
2025-05-04 02:29:25 +0200

    refactor: extract create variable

--- SnakesAndLaddersKata.Tests/SnakesLaddersShould.cs
+++ SnakesAndLaddersKata.Tests/SnakesLaddersShould.cs
@@ -5,16 +5,16 @@ namespace SnakesAndLaddersKata.Tests
 
     public class SnakesLaddersShould
     {
+        private SnakesLadders snakesLadders;
         [SetUp]
         public void Setup()
         {
+            snakesLadders = new SnakesLadders();
         }
 
         [Test]
         public void PlayerOneMove3()
         {
-            var snakesLadders = new SnakesLadders();
-
             var result = snakesLadders.Play(1, 2);
 
             result.ShouldBe("¡Jugador 1 está en la casilla 3!");
@@ -23,8 +23,6 @@ namespace SnakesAndLaddersKata.Tests
         [Test]
         public void PlayerOneMove5()
         {
-            var snakesLadders = new SnakesLadders();
-
             var result = snakesLadders.Play(3, 2);
 
             result.ShouldBe("¡Jugador 1 está en la casilla 5!");
@@ -33,7 +31,6 @@ namespace SnakesAndLaddersKata.Tests
         [Test]
         public void PlayerTwoMove3()
         {
-            var snakesLadders = new SnakesLadders();
             snakesLadders.Play(1, 2);
             
             var result = snakesLadders.Play(1, 2);
@@ -44,7 +41,6 @@ namespace SnakesAndLaddersKata.Tests
         [Test]
         public void PlayerOneMoveAnotherTimeAfterPlayer2()
         {
-            var snakesLadders = new SnakesLadders();
             snakesLadders.Play(1, 2); //Player1 to box 3
             snakesLadders.Play(1, 2); //Player2 to box 3
             
@@ -57,7 +53,6 @@ namespace SnakesAndLaddersKata.Tests
         [Ignore("paso demasiado grande")]
         public void PlayerOneMoveTwoTimesWhenIsDouble()
         {
-            var snakesLadders = new SnakesLadders();
             snakesLadders.Play(3, 1); 
             
             var result = snakesLadders.Play(1, 2);
--- SnakesAndLaddersKata/SnakesLadders.cs
+++ SnakesAndLaddersKata/SnakesLadders.cs
@@ -7,16 +7,16 @@ public class SnakesLadders
     private int _player2Score = 0;
     public string Play(int die1, int die2)
     {
-        var box = die1 + die2;
+        var score = die1 + die2;
 
         if (_player1)
         {
             _player1 = false;
-            _player1Score += box;
+            _player1Score += score;
             return $"¡Jugador 1 está en la casilla {_player1Score}!";
         }
         _player1 = true;
-        _player2Score += box;
+        _player2Score += score;
         return $"¡Jugador 2 está en la casilla {_player2Score}!";
     }
 }
