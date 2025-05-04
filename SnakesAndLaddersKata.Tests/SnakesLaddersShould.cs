1871c221d61f56db718d646ddf164dd7a87da6cb
TrikerLh <corchero100@gmail.com>
2025-05-04 02:06:33 +0200

    refactor: move code

--- SnakesAndLaddersKata.Tests/SnakesLaddersShould.cs
+++ SnakesAndLaddersKata.Tests/SnakesLaddersShould.cs
@@ -1,40 +1,33 @@
 ﻿using Shouldly;
 
-namespace SnakesAndLaddersKata.Tests;
-
-public class SnakesLaddersShould
+namespace SnakesAndLaddersKata.Tests
 {
-    [SetUp]
-    public void Setup()
+
+    public class SnakesLaddersShould
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-    [Test]
-    public void PlayerOneMove3()
-    {
-        var snakesLadders = new SnakesLadders();
+        [Test]
+        public void PlayerOneMove3()
+        {
+            var snakesLadders = new SnakesLadders();
 
-        var result = snakesLadders.Play(1, 2);
+            var result = snakesLadders.Play(1, 2);
 
-        result.ShouldBe("¡Jugador 1 está en la casilla 3!");
-    }
-    
-    [Test]
-    public void PlayerOneMove5()
-    {
-        var snakesLadders = new SnakesLadders();
+            result.ShouldBe("¡Jugador 1 está en la casilla 3!");
+        }
 
-        var result = snakesLadders.Play(3, 2);
+        [Test]
+        public void PlayerOneMove5()
+        {
+            var snakesLadders = new SnakesLadders();
 
-        result.ShouldBe("¡Jugador 1 está en la casilla 5!");
-    }
-}
+            var result = snakesLadders.Play(3, 2);
 
-public class SnakesLadders
-{
-    public string Play(int die1, int die2)
-    {
-        var box = die1 + die2;
-        return $"¡Jugador 1 está en la casilla {box}!";
+            result.ShouldBe("¡Jugador 1 está en la casilla 5!");
+        }
     }
 }
---
+++ SnakesAndLaddersKata/SnakesLadders.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakesAndLaddersKata;
+
+public class SnakesLadders
+{
+    public string Play(int die1, int die2)
+    {
+        var box = die1 + die2;
+        return $"¡Jugador 1 está en la casilla {box}!";
+    }
+}
